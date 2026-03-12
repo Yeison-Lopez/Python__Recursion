6e4f16804216ce47b4e0970ba20c86bc619788e0
yeison lopez <bogotavr@gmail.com>
2026-03-12 16:56:05 -0500

    SumOfList without comments

--- SumOfList.py
+++ SumOfList.py
@@ -1,20 +1,3 @@
-#def sum(arr):
-#    n=len(arr) #5 // 4 // 3 // 2 // 1
-#    if n==2:
-#        return add
-#    else:
-#        add=arr[n-1]+1 # arr[4]+arr[3] // arr[3]+arr[2] // arr[2]+arr[1] // arr[1]+arr[0]
-       
-#       sum( arr[:n-1] ) #[43, 3453, 3554, 56]  //  [43, 3453, 3554] // [43, 3453] // [43] 
-           
-         
-
-#list_sum=[43, 3453, 3554, 56, 567];
-
-##print(sum(list_sum));
-##print(list_sum[:3]);  --> [43, 3453, 3554]
-##print(list_sum[1:]); [3453, 3554, 56, 567]
-
 
 def sum(arr):
     if len(arr) == 0:  # Base case: empty list 
